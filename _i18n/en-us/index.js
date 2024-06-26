43688378821f437a6cb9d260ffc97ff108550671
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-26 17:53:03 -0500

    + added translations

---
+++ _i18n/en-us/index.js
@@ -0,0 +1,36 @@
+export default {
+  "Iwebhooks": {
+    "sidebar": {
+      "adminGroup": "Webhook",
+      "adminCategories": "Categories",
+      "adminHooks": "Hooks",
+      "adminLogs": "Logs"
+    },
+    "label": {
+      "dispatch": "Dispatch",
+      "noRegistration": "No registration",
+      "offline": "Offline",
+      "online": "Online",
+      "activateWebhookJob": "Enable periodic webhook calls"
+    },
+    "form": {
+      "httpMethod": "HTTP Method",
+      "httpStatus": "HTTP Status",
+      "response": "Response",
+      "endpoint": "Endpoint",
+      "actionLabel": "Action Label",
+      "callEveryMinutes": "Call Every Minute",
+      "body": "Request Body",
+      "headers": "Request Header",
+      "redirectLink": "Redirect link"
+    },
+    "title": {
+      "newCategory": "New Category",
+      "updateCategory": "Update Category",
+      "newHook": "New Hook",
+      "updateHook": "Update Hook",
+      "hook": "Hook",
+      "category": "Category"
+    }
+  }
+}
---
+++ _i18n/es/index.js
@@ -0,0 +1,36 @@
+export default {
+  "Iwebhooks": {
+    "sidebar": {
+      "adminGroup": "Webhook",
+      "adminCategories": "Categorias",
+      "adminHooks": "Hooks",
+      "adminLogs": "Registros"
+    },
+    "label": {
+      "dispatch": "Ejecutar",
+      "noRegistration": "Sin Registro",
+      "offline": "Desconectado",
+      "online": "Conectado",
+      "activateWebhookJob": "Activar llamadas periódicas a webhooks"
+    },
+    "form": {
+      "httpMethod": "Metodo HTTP",
+      "httpStatus": "Estado HTTP",
+      "response": "Respuesta",
+      "endpoint": "Url de la solicitud",
+      "actionLabel": "Label de acción",
+      "callEveryMinutes": "Llamada cada minuto",
+      "body": "Cuerpo de la solicitud",
+      "headers": "Encabezado de la solicitud",
+      "redirectLink": "Enlace de redirección"
+    },
+    "title": {
+      "newCategory": "Nueva Categoría",
+      "updateCategory": "Actualizar Categoría",
+      "newHook": "Nuevo Hook",
+      "updateHook": "Actualizar Hook",
+      "hook": "Hook",
+      "category": "Categoría"
+    }
+  }  
+}
