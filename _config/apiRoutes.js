b40e576a66f3b8a735dd604dd3cc9b684417011a
msolanogithub <msolano@imaginacolombia.com>
2024-08-01 15:19:30 -0500

    added changes to create hooks type Event

--- _config/apiRoutes.js
+++ _config/apiRoutes.js
@@ -1,12 +1,14 @@
 const moduleName = 'iwebhooks';
 const moduleVersion = 'v1';
-const urlBase = `/${moduleName}/${moduleVersion}`
+const urlBase = `/${moduleName}/${moduleVersion}`;
 
 
 export default {
-  urlBase : urlBase,
+  urlBase: urlBase,
   version: moduleVersion,
   categories: `${urlBase}/categories`,
   hooks: `${urlBase}/hooks`,
-  logs: `${urlBase}/logs`
-}
+  logs: `${urlBase}/logs`,
+  types: `${urlBase}/types`,
+  eventTypes: `${urlBase}/event-types`
+};
